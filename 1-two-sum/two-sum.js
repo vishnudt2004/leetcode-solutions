8131c396432cf1c27392d0f6c54f1d0e292babb2
Vishnu D <149229313+vishnudt2004@users.noreply.github.com>
2026-07-09 07:31:21 +0530

    Time: 166 ms (5.03%) | Memory: 54.7 MB (56.85%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 1; j < nums.length; j++) {
+            if (target === nums[i] + nums[i + j]) return [i, i + j];
+        }
+    }
+};
